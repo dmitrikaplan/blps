4d77a6539cf4414b1c09c2b4674334b933d4e883
dmitrykap1an <dk475641@gmail.com>
2024-03-27 22:21:03 +0300

    commented test

--- auth-server/src/test/kotlin/ru/kaplaan/authserver/web/controller/AuthControllerTest.kt
+++ auth-server/src/test/kotlin/ru/kaplaan/authserver/web/controller/AuthControllerTest.kt
@@ -1,4 +1,41 @@
-package ru.kaplaan.authserver.web.controller
-
-class AuthControllerTest {
-}
+//package ru.kaplaan.authserver.web.controller
+//
+//import org.junit.jupiter.api.BeforeEach
+//import org.junit.jupiter.api.Test
+//import org.junit.jupiter.api.extension.ExtendWith
+//import org.mockito.InjectMocks
+//import org.mockito.Mock
+//import org.mockito.junit.jupiter.MockitoExtension
+//import org.springframework.test.web.servlet.MockMvc
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders
+//import ru.kaplaan.authserver.service.AuthService
+//
+//@ExtendWith(MockitoExtension::class)
+//class AuthControllerTest(
+//    @Mock
+//    private val authService: AuthService,
+//
+//    @InjectMocks
+//    private val authController: AuthController
+//) {
+//
+//    lateinit var mockMvc: MockMvc
+//
+//
+//    @BeforeEach
+//    fun setUp(){
+//        mockMvc = MockMvcBuilders.standaloneSetup(authController).build()
+//    }
+//
+//
+//    @Test
+//    fun registration(){
+//        mockMvc.perform(MockMvcRequestBuilders.get("/auth/registration/user"))
+//            .andExpect(MockMvcResultMatchers.)
+//    }
+//
+//
+//
+//}
