08a11ab3e2845cf4bbeb5ca19cab2cc345fefff6
dmitrykap1an <dk475641@gmail.com>
2024-04-16 02:46:03 +0300

    the unused rabbit mq configuration has been removed

--- auth-server/src/main/kotlin/ru/kaplaan/authserver/domain/config/RabbitMqConfiguration.kt
+++ auth-server/src/main/kotlin/ru/kaplaan/authserver/domain/config/RabbitMqConfiguration.kt
@@ -1,11 +1,7 @@
 package ru.kaplaan.authserver.domain.config
 
-import org.springframework.amqp.core.*
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
-import org.springframework.amqp.rabbit.core.RabbitAdmin
-import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -19,76 +15,7 @@ class RabbitMqConfiguration {
     @Value("\${rabbit.port}")
     private var port: Int? = null
 
-    @Value("\${rabbit.user-info.get-user-id-by-username.queue-name}")
-    private lateinit var getUserIdByUsernameQueueName: String
-
-    @Value("\${rabbit.user-info.get-user-id-by-username.routing-key}")
-    private lateinit var getUserIdByUsernameRoutingKey: String
-
-    @Value("\${rabbit.user-info.get-username-by-user-id.queue-name}")
-    private lateinit var getUsernameByUserIdQueueName: String
-
-    @Value("\${rabbit.user-info.get-username-by-user-id.routing-key}")
-    private lateinit var getUsernameByUserIdRoutingKey: String
-
-    @Value("\${rabbit.user-info.get-all-usernames-by-users-id.queue-name}")
-    private lateinit var getAllUsernamesByUsersIdQueueName: String
-
-    @Value("\${rabbit.user-info.get-all-usernames-by-users-id.routing-key}")
-    private lateinit var getAllUsernamesByUsersIdRoutingKey: String
-
-    @Value("\${rabbit.user-info.exchange-name}")
-    private lateinit var userDataExchangeName: String
-
     @Bean
     fun connectionFactory(): ConnectionFactory =
         CachingConnectionFactory(hostname, port!!)
-
-    @Bean
-    fun amqpAdmin(): AmqpAdmin =
-        RabbitAdmin(connectionFactory())
-
-    @Bean
-    fun rabbitTemplate(): RabbitTemplate =
-        RabbitTemplate(connectionFactory())
-
-    @Bean
-    fun userDataExchange(): DirectExchange =
-        DirectExchange(userDataExchangeName)
-
-    @Bean
-    fun getUserIdByUsernameQueue(): Queue =
-        Queue(getUserIdByUsernameQueueName)
-
-    @Bean
-    fun getUsernameByUserIdQueue(): Queue =
-        Queue(getUsernameByUserIdQueueName)
-
-    @Bean
-    fun getAllUsernamesByUsersIdQueue(): Queue =
-        Queue(getAllUsernamesByUsersIdQueueName)
-
-
-    @Bean
-    fun getUserIdByUsernameBinding(@Qualifier("getUserIdByUsernameQueue") queue: Queue, directExchange: DirectExchange): Binding =
-        BindingBuilder
-            .bind(queue)
-            .to(directExchange)
-            .with(getUserIdByUsernameRoutingKey)
-
-
-    @Bean
-    fun getUsernameByUserIdBinding(@Qualifier("getUsernameByUserIdQueue") queue: Queue, directExchange: DirectExchange): Binding =
-        BindingBuilder
-            .bind(queue)
-            .to(directExchange)
-            .with(getUsernameByUserIdRoutingKey)
-
-
-    @Bean
-    fun getAllUsernamesByUsersIdBinding(@Qualifier("getAllUsernamesByUsersIdQueue") queue: Queue, directExchange: DirectExchange): Binding =
-        BindingBuilder
-            .bind(queue)
-            .to(directExchange)
-            .with(getAllUsernamesByUsersIdRoutingKey)
 }
