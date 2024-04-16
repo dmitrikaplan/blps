74581e788fa246bab6410619597b0a7cb7114add
dmitrykap1an <dk475641@gmail.com>
2024-04-16 17:47:48 +0300

    fixed an error with incorrect import of the @Transient annotation

--- consumer-server/src/main/kotlin/ru/kaplaan/consumer/domain/entity/data/UserData.kt
+++ consumer-server/src/main/kotlin/ru/kaplaan/consumer/domain/entity/data/UserData.kt
@@ -1,6 +1,7 @@
 package ru.kaplaan.consumer.domain.entity.data
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.Transient
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import java.time.LocalDate
