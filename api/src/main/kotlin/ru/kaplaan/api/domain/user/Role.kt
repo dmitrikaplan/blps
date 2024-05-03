4d5a0ff56756a7df160f50160d7945ee2b2e8c39
dmitrykap1an <dk475641@gmail.com>
2024-05-03 17:29:49 +0300

    added ROLE_ADMIN

--- api/src/main/kotlin/ru/kaplaan/api/domain/user/Role.kt
+++ api/src/main/kotlin/ru/kaplaan/api/domain/user/Role.kt
@@ -3,7 +3,7 @@ package ru.kaplaan.api.domain.user
 import org.springframework.security.core.GrantedAuthority
 
 enum class Role: GrantedAuthority{
-    ROLE_USER, ROLE_COMPANY;
+    ROLE_USER, ROLE_COMPANY, ROLE_ADMIN;
     override fun getAuthority(): String {
         return this.name
     }
