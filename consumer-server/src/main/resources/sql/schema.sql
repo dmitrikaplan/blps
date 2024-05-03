e66e200c9e37401d97c8c5052e75e9752dfdb030
dmitrykap1an <dk475641@gmail.com>
2024-05-03 17:43:39 +0300

    consumer-server: added field purpose_of_payment to payment_order table

--- consumer-server/src/main/resources/sql/schema.sql
+++ consumer-server/src/main/resources/sql/schema.sql
@@ -81,6 +81,7 @@ create table if not exists payment_order(
     recipient_bank_account_number text not null check(length(recipient_bank_account_number) = 20),
     recipient_bank_name text not null,
     creation_date date not null,
+    purpose_of_payment text not null check(length(purpose_of_payment) > 0),
     is_completed bool not null
 );
 
