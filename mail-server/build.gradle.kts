4ac6b1b6a62f687127d2361ae8181b46cc76908f
dmitrykap1an <dk475641@gmail.com>
2024-05-10 23:12:02 +0300

    mail-server: war changed to jar

--- mail-server/build.gradle.kts
+++ mail-server/build.gradle.kts
@@ -1,7 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    war
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.23"
