9235a66278aa6c475146ba1d9712c9c725a82e0d
dmitrykap1an <dk475641@gmail.com>
2024-05-21 19:37:55 +0300

    changed quartz configuration

--- consumer-server/src/main/kotlin/ru/kaplaan/consumer/domain/config/quartz/job/sendPaymentOrder/SendPaymentOrderJobConfig.kt
+++ consumer-server/src/main/kotlin/ru/kaplaan/consumer/domain/config/quartz/job/sendPaymentOrder/SendPaymentOrderJobConfig.kt
@@ -4,17 +4,19 @@ import org.quartz.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import ru.kaplaan.consumer.domain.properties.quartz.SchedulerProperties
+import ru.kaplaan.consumer.domain.properties.quartz.SendPaymentOrderProperties
 
 @Configuration
 class SendPaymentOrderJobConfig(
-    private val schedulerProperties: SchedulerProperties
+    private val schedulerProperties: SchedulerProperties,
+    private val sendPaymentOrderProperties: SendPaymentOrderProperties
 ) {
 
     @Bean
     fun sendPaymentOrderJob(): JobDetail =
         JobBuilder
             .newJob(SendPaymentOrderJob::class.java)
-            .withIdentity("sendPaymentOrder", schedulerProperties.permanentJobsGroupName)
+            .withIdentity(sendPaymentOrderProperties.jobName, schedulerProperties.permanentJobsGroupName)
             .storeDurably()
             .requestRecovery(true)
             .build()
@@ -24,7 +26,7 @@ class SendPaymentOrderJobConfig(
         TriggerBuilder
             .newTrigger()
             .forJob(sendPaymentOrderJob())
-            .withIdentity("sendPaymentOrderTrigger", schedulerProperties.permanentJobsGroupName)
+            .withIdentity(sendPaymentOrderProperties.triggerName, schedulerProperties.permanentJobsGroupName)
             .withSchedule(CronScheduleBuilder.cronSchedule(schedulerProperties.sendPaymentOrderJobCron))
             .build()
 }
